d2fe73994556b0092f9102ccf92a4f66f03cceb9
Reiker <reiker1@hotmail.com>
2022-10-16 10:11:49 +0200

    retoqueMenor

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
  let booleano1 = true;
  let booleano2 = false;
  // 3.- Crear variable tipo const de nombre PI declarada con valor 3.14
- const PI = Math.PI;
+ const PI = 3.14;
  // 4.- Crear variable tipo const de nombre TAU declarada con valor 2 veces PI
- const TAU = Math.PI*2;
+ const TAU = PI*2;
  // 5.- Crear variable booleanoAnd cuyo valor sea la comparación booleana booleano1 and booleano2
- let booleanoAnd = booleano1 === booleano2;
+ let booleanoAnd = booleano1 && booleano2;
  // 6.- Crear variable booleanoNot cuyo valor sea la compracación booleana no booleano1
  let booleanoNot = !booleano1;
  // 7.- Crear variable booleanoMix0 cuyo valor sea la compración booleana (booleano1 or booleano2) and (booleano1 or (not booleano1 and not booleano2)
@@ -20,8 +20,8 @@
  let incrementarAntes = 2;
  let resultadoAntes = ++incrementarAntes;
  // 10.- Crear variable contarHasta10_2 con valor 0 e incrementar su valor con un bucle for hasta que se verifique que contarHasta10_2 === 10
-  
- for (let contarHasta10_2 = 0; contarHasta10_2 <= 10; contarHasta10_2++) {
+let contarHasta10_2 = 0
+ for (; contarHasta10_2 < 10; contarHasta10_2++) {
      
  }
  //11.- Crear las variables postI y postJ con valor 0 a continuación cree un bucle que itere 11 veces. En cada iteración se deberá sumar al valor de postI el valor de postJ++
@@ -51,7 +51,7 @@
  // 17.- Crear variable booleanoMix1 cuyo valor sea la comparación booleana (booleano1 and (TAU/2 sea igual a PI)) or (variableValorNumerico mayor o igual que MiNumeroFav)
  let booleanoMix1 = (booleano1 && (TAU / 2 === PI)) || (variableValorNumerico >= MiNumeroFav);
  // 18.- Crear variable seisNoEsNueve cuyo valor sea la comparación booleana 6 no es estrictamente igual que 9
- let seisNoEsNueve = 6 == 9  
+ let seisNoEsNueve = 6 !== 9  
  // 19.- Crear variable booleanoMix2 cuyo valor sea la comparación booleana variableValorNumerico positivo (0 no incluido) o menor que -(MiNumeroFav * TAU)
  let booleanoMix2 = variableValorNumerico > 0 || variableValorNumerico > (MiNumeroFav * TAU);
  // 20.- Crear variable valorSuma cuyo valor sea la suma de MiNumeroFav y variableValorNumerico
@@ -59,12 +59,12 @@
  // 21.- Crear variable valorResta cuyo valor sea la resta de MiNumeroFav y variableValorNumerico
  let valorResta = MiNumeroFav - variableValorNumerico
  // 22.- Crear variable valorMultiplicación cuyo valor sea la multiplicación de MiNumeroFav por variableValorNumeric
- let valorMultiplicación = MiNumeroFav * variableValorNumerico
+ let valorMultiplicacion = MiNumeroFav * variableValorNumerico
  // 23.- Crear variable valorDivisión cuyo valor sea la división de MiNumeroFav entre 3
- let valorDivisión = MiNumeroFav / 3
+ let valorDivision = MiNumeroFav / 3
  //24.- Crear variable contarHasta10 con valor 0 e incrementar su valor con un bucle while hasta que se verifique que contarHasta10 === 10
  let contarHasta10 = 0
- while (contarHasta10 <= 10) { 
+ while (contarHasta10 < 10) { 
     // console.log(contarHasta10)
      contarHasta10++;
  } 
